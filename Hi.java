7fed1f933d615d2d7e5c2f648498ff6c8fdf88d4
pat_mic <pat_mic@hotmail.com>
2026-04-07 08:48:45 -0500

    Add additional greeting message in Hi.java
    saludos

--- Hi.java
+++ Hi.java
@@ -2,5 +2,6 @@ public class Hi {
     public static void main(String[] args) {
         System.out.println("Hi, amo!");
         System.out.println("Hi, pat_mic!");
+        System.out.println("todo biend desde la nube, pat_mic!");
     }
 }
