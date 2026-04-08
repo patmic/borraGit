48b1601f030677a6a2f685d7079befdfecfd1e61
pat_mic <pat_mic@hotmail.com>
2026-04-08 08:50:19 -0500

    Fix: cambio del nick

--- src/Hi.java
+++ src/Hi.java
@@ -2,6 +2,6 @@ package src;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("Hi, amo!");
-        System.out.println("Hi, pat_mic!");
+        System.out.println("Hi, patricio!");
     }
 }
